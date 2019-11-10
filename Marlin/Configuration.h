0d4ff994fb1d1a3a6ede8b53b0915221e1b9c05a
Tomer Orian <tomer@Tomers-MacBook-Pro.local>
2019-11-10 19:20:13 +0200

    reduce bed Y to prevent crashing with camera

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -882,7 +882,7 @@
 
 // The size of the print bed
 #define X_BED_SIZE 220
-#define Y_BED_SIZE 236
+#define Y_BED_SIZE 220
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0

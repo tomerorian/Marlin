650c11958ce2206d6911bb2f6c6fc1a6c86690a4
Tomer Orian <tomer@Tomers-MacBook-Pro.local>
2019-11-11 09:16:26 +0200

    robo doesnt need clearance, now it probes the borders too

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -783,7 +783,7 @@
 #define Z_PROBE_OFFSET_FROM_EXTRUDER 1.26   // Z offset: -below +above  [the nozzle]
 
 // Certain types of probes need to stay away from edges
-#define MIN_PROBE_EDGE 5
+#define MIN_PROBE_EDGE 0
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 16000
@@ -1060,8 +1060,8 @@
 
   //#define MESH_EDIT_GFX_OVERLAY   // Display a graphics overlay while editing the mesh
 
-  #define MESH_INSET 1              // Set Mesh bounds as an inset region of the bed
-  #define GRID_MAX_POINTS_X 10      // Don't use more than 15 points per axis, implementation limited.
+  #define MESH_INSET 0              // Set Mesh bounds as an inset region of the bed
+  #define GRID_MAX_POINTS_X 7      // Don't use more than 15 points per axis, implementation limited.
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   #define UBL_MESH_EDIT_MOVES_Z     // Sophisticated users prefer no movement of nozzle
